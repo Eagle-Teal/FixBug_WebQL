53edff8dafaa44b27978fc3ef60c7404f74ae5da
Thắng Trương Bồ Quốc <quocthangngoc14@gmail.com>
2022-07-07 11:56:57 +0700

    uploaded

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Error_404 from "./pages/404";
 
+// CSS PAGE
+import "./pages/assets/vendors/mdi/css/materialdesignicons.min.css";
+import "./pages/assets/vendors/css/vendor.bundle.base.css"
+import "./pages/assets/css/style.css"
+import "./pages/assets/images/favicon.png"
+
+function WebQL() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          {/* <Route index element={<Homes />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+          <Route index element={<Login />} />
+          <Route path="*" element={<Error_404 />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<WebQL />);
